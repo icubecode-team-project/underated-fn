b008f094563d09ec887b306219e39c9d578d3de4
9999-balasu <138554962+9999-balasu@users.noreply.github.com>
2025-04-21 15:58:21 +0530

    Updated footer to use company logo instead of Amazon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+ 
+import Logo from '../assets/logo-icon.png';
 
 const Footer = () => {
   return (
@@ -11,7 +13,7 @@ const Footer = () => {
             <h3 className="text-white font-semibold mb-2">Help</h3>
             <ul className="space-y-1">
               <li><Link to="#" className="hover:text-white">Site</Link></li>
-              <li><Link to="#" className="hover:text-white">IMDb</Link></li>
+              <li><Link to="#" className="hover:text-white">underated.com </Link></li>
               <li><Link to="#" className="hover:text-white">Box Office Mojo</Link></li>
               <li><Link to="#" className="hover:text-white">License</Link></li>
             </ul>
@@ -20,7 +22,7 @@ const Footer = () => {
           <div>
             <h3 className="text-white font-semibold mb-2">More</h3>
             <ul className="space-y-1">
-              <li><Link to="#" className="hover:text-white">IMDbPro</Link></li>
+              <li><Link to="#" className="hover:text-white">underated.com Pro</Link></li>
               <li><Link to="#" className="hover:text-white">Press Room</Link></li>
               <li><Link to="#" className="hover:text-white">Advertising</Link></li>
               <li><Link to="#" className="hover:text-white">Jobs</Link></li>
@@ -37,10 +39,17 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div className="flex flex-col justify-center">
+          {/*<div className="flex flex-col justify-center">
             <p className="text-white font-semibold">An Amazon Company</p>
-            <p className="mt-1 text-xs">© 1990–2025 by IMDb.com, Inc.</p>
-          </div>
+            <p className="mt-1 text-xs">© 1990–2025 by underated.com, Inc.</p>
+          </div>*/}
+         
+
+<div className="flex flex-col justify-center items-center">
+  <img src={Logo} alt="Company Logo" className="w-20 h-auto mb-2" />
+  <p className="mt-1 text-xs text-white">© 1990–2025 by underated.com, Inc.</p>
+</div>
+
 
         </div>
       </div>
