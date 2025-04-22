8fd71e0fbb70b95af603a0e458e3d83fb06ccd76
9999-balasu <138554962+9999-balasu@users.noreply.github.com>
2025-04-22 19:13:47 +0530

    Resolved conflicts and updated feature/44046

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,13 +2,13 @@
 
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "../Header"; // Import the Header component
+
 import Footer from "../Footer";
 
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header /> {/* Add Header at the top */}
+      
       <main className="flex-grow">
         <Outlet /> {/* This is where nested routes will be rendered */}
       </main>
