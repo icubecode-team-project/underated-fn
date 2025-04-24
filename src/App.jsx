0bc77079575879fb41f0b0145ddd5966df1b89f4
leela <leeladurga8@gmail.com>
2025-04-24 17:30:46 +0530

    Login page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./components/Layout";
 import "./App.css";
+import Login from "./pages/Login";
+
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         {/* Add more nested routes here like about, contact etc...*/}
       </Route>
       {/* Add more Routes here like login, logout etc..*/}
+      <Route path="/login" element={<Login/>}/>
+      
     </Routes>
   );
 }
