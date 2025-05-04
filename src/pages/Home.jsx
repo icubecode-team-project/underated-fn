5caef42772f6eb8daecd7cc6b51e0867f462357d
Sureshchalla-123 <sureshchalla9876@gmail.com>
2025-05-05 03:58:44 +0530

    fetched data from backedn api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ImageSlider from "../components/ImageSlider/imageSlider.jsx";
 import MovieCards from "../components/MovieCards/";
+import { OPTIONS } from "../assets/constants.js";
 
 const Home = () => {
   //throw new Error("Oops");
+
+  useEffect(() => {
+    getMovieDetails();
+  }, []);
+
+  const getMovieDetails = async () => {
+    const url = "http://localhost:8080/api/v1/movie/get/all-movies";
+    const response = await fetch(url, OPTIONS);
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <>
       {/* <h1 className="text-3xl text-blue-500 text-center">Header</h1> */}
