31862c01b7943fbfcd6fbf4b62b6c6954992b7e7
Sureshchalla-123 <sureshchalla9876@gmail.com>
2025-05-17 10:31:10 +0530

    added rating pop over

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "../Footer";  // Import the Footer component
+import Footer from "../Footer"; // Import the Footer component
 import Header from "../Header";
+import RatingModel from "../RatingModel";
 
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+      <RatingModel />
       <main className="flex-grow">
         <Outlet />
       </main>
